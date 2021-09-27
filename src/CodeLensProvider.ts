d04783be9b7754dc8b69785a38e9372756f57261
Carsten Daurehøj <arelstone@gmail.com>
2021-09-27 13:49:18 +0200

    Add simple codelens

---
+++ src/CodeLensProvider.ts
@@ -0,0 +1,56 @@
+import * as vscode from 'vscode';
+
+/**
+ * CodelensProvider
+ */
+export class CodelensProvider implements vscode.CodeLensProvider {
+
+    private codeLenses: vscode.CodeLens[] = [];
+    private regex: RegExp;
+    private _onDidChangeCodeLenses: vscode.EventEmitter<void> = new vscode.EventEmitter<void>();
+    public readonly onDidChangeCodeLenses: vscode.Event<void> = this._onDidChangeCodeLenses.event;
+
+    constructor() {
+        // this.regex = /(.+)/g;
+        this.regex = /I18n\.t\('(\w*)'/gmi;
+
+        vscode.workspace.onDidChangeConfiguration((_) => {
+            this._onDidChangeCodeLenses.fire();
+        });
+    }
+
+    public provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
+
+        if (vscode.workspace.getConfiguration("whats-the-text").get("enable", true)) {
+            this.codeLenses = [];
+            const regex = new RegExp(this.regex);
+            const text = document.getText();
+
+            let matches;
+            while ((matches = regex.exec(text)) !== null) {
+                const line = document.lineAt(document.positionAt(matches.index).line);
+                const indexOf = line.text.indexOf(matches[0]);
+                const position = new vscode.Position(line.lineNumber, indexOf);
+                const range = document.getWordRangeAtPosition(position, new RegExp(this.regex));
+                if (range) {
+                    this.codeLenses.push(new vscode.CodeLens(range));
+                }
+            }
+            return this.codeLenses;
+        }
+        return [];
+    }
+
+    public resolveCodeLens(codeLens: vscode.CodeLens, token: vscode.CancellationToken) {
+        if (vscode.workspace.getConfiguration("whats-the-text").get("enable", true)) {
+            codeLens.command = {
+                title: "THIS IS A TRANSLATED VALUE",
+                // tooltip: "Tooltip provided by sample extension",
+                command: "whats-the-text.codelensAction",
+                arguments: ["Argument 1", false]
+            };
+            return codeLens;
+        }
+        return null;
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { CodelensProvider } from './CodeLensProvider';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "whats-the-text" is now active!');
+let disposables: vscode.Disposable[] = [];
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('whats-the-text.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from whats-the-text!');
+export function activate(context: vscode.ExtensionContext) {	
+	const codelensProvider = new CodelensProvider();
+
+	vscode.languages.registerCodeLensProvider(
+		[
+			{ language: 'javascript' },
+			{ language: 'javascriptreact' },
+			{ language: 'typescript' },
+			{ language: 'typescriptreact' },
+		],
+		codelensProvider
+	);
+
+	vscode.commands.registerCommand('whats-the-text.enable', () => {
+		vscode.workspace.getConfiguration('whats-the-text').update('enable', true, true);
 	});
 
-	context.subscriptions.push(disposable);
+	vscode.commands.registerCommand('whats-the-text.disable', () => {
+		vscode.workspace.getConfiguration('whats-the-text').update('enable', false, true);
+	});
+
+	vscode.commands.registerCommand('whats-the-text.codelensAction', (args: any) => {
+		vscode.window.showInformationMessage(`CodeLense action clicked with args=${args}`);
+	});
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	if (disposables) {
+		disposables.forEach(item => item.dispose());
+	}
+
+	disposables = [];
+}
